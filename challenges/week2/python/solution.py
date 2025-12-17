e76fbfee3e402283faae560bbd33b75b886e0b17
Mario Marín <mariomh2005@gmail.com>
2025-12-17 17:09:37 +0100

    week 2 xmas market challenge done

--- challenges/week2/python/solution.py
+++ challenges/week2/python/solution.py
@@ -9,4 +9,74 @@ def classify_arch(arch: list[list[int]]) -> str:
     Returns:
         str: Name of the arch period as it appears on challenge.md.
     """
-    # TODO: Write your code here
+    
+    patterns = {
+        "Romanos": [
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        ],
+        "Abderraman I": [
+            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+            [0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1],
+            [0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        ],
+        "Abderraman II": [
+            [0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+        ],
+        "Al-Hakam II": [
+            [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        ],
+        "Almanzor": [
+            [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+        ]
+    }
+
+    best_match = None
+    max_matches = -1
+
+    input_rows = len(arch)
+    if input_rows == 0:
+        return ""
+    input_cols = len(arch[0])
+
+    for name, pattern in patterns.items():
+        pattern_rows = len(pattern)
+        if pattern_rows != input_rows:
+            continue
+        
+        pattern_cols = len(pattern[0])
+        match_cols = min(input_cols, pattern_cols)
+        
+        current_matches = 0
+        
+        for r in range(input_rows):
+            for c in range(match_cols):
+                if arch[r][c] == pattern[r][c]:
+                    current_matches += 1
+        
+        if current_matches > max_matches:
+            max_matches = current_matches
+            best_match = name
+            
+    return best_match
